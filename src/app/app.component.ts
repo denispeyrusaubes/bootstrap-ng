e0a094cad790974a93b7c07373411246638b8356
thomas peyrusaubes <thomas.peyrusaubes@ynov.com>
2025-08-30 15:12:43 +0200

    feat: add home button with Material design to the navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 
@@ -8,7 +9,7 @@ type Section = { titre: string; description: string };
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],              // ngFor vient d'ici
+  imports: [RouterOutlet,RouterLink,MatIcon],              // ngFor vient d'ici
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
